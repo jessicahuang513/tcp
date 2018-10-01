0bb98017950f5b825bc0e4505761cebd2970839e
Jessica Huang <jhuang@jessicas-mbp.home>
2018-10-01 00:31:57 -0500

    new files

---
+++ assets/js/typewriter.js
@@ -0,0 +1,8 @@
+$(function(){
+      $("#mainText").typed({
+        strings: ["Sending love your way...", "Sign up today!"],
+        typeSpeed: 110, // typing speed
+        backDelay: 500, // pause before backspacing
+        loop: true,
+      });
+  });
